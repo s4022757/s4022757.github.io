fe2815c1eeeb48f0846ba2494fbd25f05ca4bb98
s4022757 <s4022757@student.rmit.edu.au>
2026-05-13 20:49:01 +1000

    VISsetup

---
+++ VISscript.js
@@ -0,0 +1,51 @@
+const button1 = document.getElementById('button1')
+let audio1 = new Audio('assets/sword.wav');
+/* audio1.src = 'assets/sword-sound.wav'; */
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+console.log(audioCtx);
+
+button1.addEventListener('click', function(){
+    console.log('click');
+    audio1.play();
+    audio1.addEventListener('playing', function(){
+        console.log('Audio 1 started playing')
+    });
+    audio1.addEventListener('ended', function(){
+        console.log('Audio 1 ended');
+    });
+});
+
+const button2 = document.getElementById('button2');
+button2.addEventListener('click', playSound);
+function playSound(){
+    const oscillator = audioCtx.createOscillator();
+    oscillator.connect(audioCtx.destination)
+    oscillator.type = 'sine';
+    oscillator.start();
+    setTimeout(function (){
+        oscillator.stop();
+    }, 1000);
+    /* audio1.play(); */
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* browser fix from gemini
+playBtn.addEventListener('click', () => {
+    // This is the "magic" line for cross-browser compatibility
+    if (audioCtx.state === 'suspended') {
+        audioCtx.resume();
+    }
+    togglePlayPause();
+});
+*/
---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+
+
+video.addEventListener('play', () => {
+    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v1.png";
+});
+
+video.addEventListener('pause', () => {
+    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
+});
